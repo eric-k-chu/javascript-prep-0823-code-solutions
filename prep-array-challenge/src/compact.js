52630fe526e4fd52584fa77333b98f0412673fe0
eric-k-chu <kiyuen88ec@gmail.com>
2023-08-16 03:32:57 +0000

    Finished prep-array-challenge exercise

---
+++ prep-array-challenge/src/compact.js
@@ -0,0 +1,10 @@
+/* exported compact */
+function compact(array) {
+  const arr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]) {
+      arr.push(array[i]);
+    }
+  }
+  return arr;
+}
