6528736dc30affaec3de6fd20522ea079c88cb71
eric-k-chu <kiyuen88ec@gmail.com>
2023-08-18 02:27:40 +0000

    Finished prep-object-challenge exercise

---
+++ prep-object-challenge/src/to-object.js
@@ -0,0 +1,9 @@
+/* exported toObject */
+function toObject(keyValuePair) {
+  const key = keyValuePair[0];
+  const value = keyValuePair[1];
+  const newObj = {};
+  newObj[key] = value;
+
+  return newObj;
+}
